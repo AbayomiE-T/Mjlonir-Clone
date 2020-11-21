8a941c12dd277354ebbbec0039cda1f2ff5c45a4
Abayomi <eyitouyoa3@gmail.com>
2020-11-21 09:45:24 -0500

    refactored some of the css

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function Brewery() {
     return (
-        <div>
-
-        </div>
+        <section id="Brewery">
+            <div className="bg-image brewery"></div>
+            <div className="bg-darken">
+                <div className="bg-text">
+                    <div className="line line-1"></div>
+                    <div className="line line-2"></div>
+                    <div className="line line-3"></div>
+                    <div className="line line-4"></div>
+                    <p>THE MJLONIR EXPERIENCE</p>
+                </div>
+            </div>
+        </section>
     )
 }
