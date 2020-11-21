9e881d53573f77bc181f2c58ef7dd254a0030e8b
Abayomi <eyitouyoa3@gmail.com>
2020-11-21 15:31:05 -0500

    Events sections needs some more work

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import karat from '../Assets/Images/Karat-event.png'
+import hzevent from '../Assets/Images/hz-beer.jpg'
+import retro from '../Assets/Images/retro-beer-festival.jpg'
+import guild from '../Assets/Images/brewers-guild.jpg'
 
 export default function Events() {
     return (
-        <div>
+        <section id="Events">
+            <div className="bg-image events"></div>
+            <div className="bg-darken event-bg">
+                <div className="bg-text">
+                    <div className="line line-1"></div>
+                    <div className="line line-2"></div>
+                    <div className="line line-3"></div>
+                    <div className="line line-4"></div>
+                    <p>YOUR NEXT EXPERIENCE</p>
+                </div>
+            </div>
 
-        </div>
+            <div className="event-links">
+                <span className="">EVENT</span>
+                <span className="">LOCATION</span>
+                <span className="">DATE</span>
+                <span className="">SEARCH</span>
+            </div>
+
+            <div className="upcoming-events">
+                <div className="event">
+                    <img src={karat} alt="" />
+                </div>
+                <div className="event">
+                    <img src={hzevent} alt="" />
+                </div>
+                <div className="event">
+                    <img src={retro} alt="" />
+                </div>
+                <div className="event">
+                    <img src={guild} alt="" />
+                </div>
+            </div>
+        </section>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
             <ul className="navigation">
                 <li><a href="#About" className="nav-item">About</a></li>
                 <li><a href="#Brewery" className="nav-item">Brewery</a></li>
-                <li><a href="Events" className="nav-item">Events</a></li>
+                <li><a href="#Events" className="nav-item">Events</a></li>
             </ul>
         </nav>
     )
