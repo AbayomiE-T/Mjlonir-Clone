667a3de77592814b22227c9040068889cd820fcb
Abayomi <eyitouyoa3@gmail.com>
2020-11-22 13:45:28 -0500

    There is now smooth scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,47 @@
 import React from 'react'
 import logo from '../Assets/Beer/Logo.png'
+import { Link, animateScroll as scroll } from 'react-scroll'
 
 export default function Navbar() {
     return (
         <nav>
             <div id="logo">
-                <a href="#Home"><img src={logo} alt="" /></a>
+                <Link className="nav-item" to="Home"><img src={logo} alt="" /></Link>
             </div>
 
             <div className="rule"></div>
 
             <ul className="navigation">
-                <li><a href="#About" className="nav-item">About</a></li>
-                <li><a href="#Brewery" className="nav-item">Brewery</a></li>
-                <li><a href="#Events" className="nav-item">Events</a></li>
+                <li>
+                    <Link className="nav-item" to="About"
+                        activeClass="active"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                    >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-item" to="Brewery"
+                        activeClass="active"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                    >
+                        Brewery
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-item" to="Events"
+                        activeClass="active"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                    >
+                        Events
+                    </Link>
+                </li>
             </ul>
         </nav>
     )
